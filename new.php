768490bbbd56fda81f0f5ccec89b94e0d7d9fc9a
nuretdinov <nured@yandex.ru>
2021-03-04 18:06:04 +0300

    new

--- new.php
+++ new.php
@@ -1,6 +1,17 @@
 <?php
-$json_user = '{"nick":"nured", "email":"nured@yandex.ru", "tel":"89213259648"}';
-$rez = json_decode($json_user);
-print $rez;
+$json_user = '{"nick":"admin", "email":"admin@yandex.ru", "tel":"8-800-555-0000"}';
 
+//преобразуем json в ассоциативный массив и выводим
+$rez1 = json_decode($json_user, true); //true – для преобразования в ассоц.массив
+foreach($rez1 as $rez) echo $rez;
+
+//преобразуем json в класс и вывобдим
+$rez2 = json_decode($json_user);
+echo $rez2->nick . $rez2->email . $rez2->tel;
+
+//массив преобразуем в json
+$new_array = array("admin", "admin@yandex.ru", "8-800-555-0000"); //индексный
+$new_array = array("nick"=>"admin", "email"=>"admin@yandex.ru", "tel"=>"8-800-555-0000"); //ассоциативный
+$rez3 = json_encode($new_array);
+print_r($rez3);
 ?>
